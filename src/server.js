1fe73de9adfc54b261968f32909a489642f6f7fe
Yerson León <yelemo777@gmail.com>
2021-12-06 13:45:58 -0500

    Ajustando boostrap, font awsome, barra de navegacion

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 //initializations
 const app = express();
 
 //settings
 app.set('port',process.env.PORT || 4000);
 app.set('views',path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 //middlewars
 app.use(express.urlencoded({ extended: false }));
@@ -14,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 
 //routes
 app.get('/', (req,res) => {
-    res.send("Hello wooooooorld ");
+    res.render('index');
 });
 
 //static files
