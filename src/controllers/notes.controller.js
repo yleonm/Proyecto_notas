5d568e55b70192bcbbf61b0ddc3298e04ab2d186
Yerson León <yelemo777@gmail.com>
2021-12-07 15:13:05 -0500

    renderizando lista notas

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,16 +1,22 @@
 const notesCtrl = {};
 
+const Note = require('../models/Note');
+
 notesCtrl.renderNoteForm = (req, res) => {
     res.render('notes/new-note');
 };
 
-notesCtrl.createNewNote = (req, res) => {
-    console.log(req.body);
+notesCtrl.createNewNote = async (req, res) => {
+    const { title, description } = req.body;
+    const newNote = new Note({ title, description});
+    await newNote.save();
+    
     res.send('new note');
 };
 
-notesCtrl.renderNotes = (req, res) => {
-    res.send('render notes');
+notesCtrl.renderNotes = async (req, res) => {
+    const notes = await Note.find().lean();
+    res.render('notes/all-notes',{notes});
 };
 
 notesCtrl.renderEditForm = (req, res) => {
