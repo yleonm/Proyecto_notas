9213b07263d8338a3336466baecc7c01d18593c8
Yerson León <yelemo777@gmail.com>
2021-12-10 23:35:53 -0500

    Mensajes con connect flash

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -10,6 +10,7 @@ notesCtrl.createNewNote = async (req, res) => {
     const { title, description } = req.body;
     const newNote = new Note({ title, description});
     await newNote.save();
+    req.flash('success_message','Note Added Succesfully');
     res.redirect('/notes');
 };
 
@@ -27,11 +28,13 @@ notesCtrl.renderEditForm = async (req, res) => {
 notesCtrl.updateNote = async (req, res) => {
     const { title, description } = req.body; 
     await Note.findByIdAndUpdate(req.params.id, {title, description});
+    req.flash('success_message','Note Updated Successfully');
     res.redirect('/notes');
 };
 
 notesCtrl.deleteNote = async (req, res) => {
     await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_message','Note Deleted Successfully');
     res.redirect('/notes')
 }
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 //initializations
 const app = express();
 
@@ -22,8 +25,18 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false }));
 app.use(methodOverride('_method'));
 
-//global variables
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
 
+//global variables
+app.use((req, res, next) => {
+    res.locals.success_message = req.flash('success_message');
+    next();
+});
 //routes
 app.use(require('./routes/index.routes'));
 app.use(require('./routes/notes.routes'));
