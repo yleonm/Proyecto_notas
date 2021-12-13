9d2d831c9e23f008c2f830dca1313dee9d6aa6dc
Yerson León <yelemo777@gmail.com>
2021-12-13 12:47:32 -0500

    Proteger las rutas de express passport

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -10,20 +10,22 @@ const {
   deleteNote
 } = require("../controllers/notes.controller");
 
-//New note
-router.get('/notes/add', renderNoteForm);
+const {isAuthenticated} = require('../helpers/auth')
 
-router.post('/notes/new-note', createNewNote);
+//New note
+router.get('/notes/add',isAuthenticated,renderNoteForm);
+
+router.post('/notes/new-note',isAuthenticated, createNewNote);
 
 //Get All Note
-router.get("/notes", renderNotes);
+router.get("/notes",isAuthenticated, renderNotes);
 
 //edit notes
-router.get("/notes/edit/:id", renderEditForm);
+router.get("/notes/edit/:id",isAuthenticated, renderEditForm);
 
-router.put("/notes/edit/:id", updateNote);
+router.put("/notes/edit/:id",isAuthenticated, updateNote);
 
 //Delete notes
-router.delete("/notes/delete/:id", deleteNote);
+router.delete("/notes/delete/:id",isAuthenticated, deleteNote);
 
 module.exports = router;
